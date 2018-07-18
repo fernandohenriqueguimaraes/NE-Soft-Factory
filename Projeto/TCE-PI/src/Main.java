061a0386c9756458abed95245a9467b985b2c265
Fernando Henrique Guimarães <fernandohenriqueguimaraes@gmail.com>
2018-07-18 19:35:47 -0300

    Buscador DO PI
    Código funcional para busca dos diários oficiais do DO PI

--- Projeto/TCE-PI/src/Main.java
+++ Projeto/TCE-PI/src/Main.java
@@ -18,10 +18,13 @@ import static java.util.Objects.isNull;
 public class Main {
 
     // URL das fontes disponibilizadas para busca dos diários oficiais
-    public final static String DIARIO_OFICIAL_PI = "http://www.diariooficial.pi.gov.br/diarios.php";
     public final static String DIARIO_OFICIAL_DOS_MUNICIPIOS = "http://www.diarioficialdosmunicipios.org/";
     public final static String DOM_TERESINA = "http://www.dom.teresina.pi.gov.br/lista_diario.php";
     public final static String DOM_PARNAIBA = "http://dom.parnaiba.pi.gov.br/";
+    public final static String DIARIO_OFICIAL_PI = "http://www.diariooficial.pi.gov.br/diarios.php";
+    
+    // URL Específica para buscar diarios oficiais do Diário Oficial do PI
+    public final static String DIARIO_OFICIAL_PI_ESPECIFICA = "http://www.diariooficial.pi.gov.br/diario.php?dia=";
 
     // Lista do objeto Fontes em que consta a data e o nome do pdf
     static ArrayList<Diarios> diarios = new ArrayList<Diarios>();
@@ -53,7 +56,7 @@ public class Main {
             URL url = new URL(urlString);
             BufferedReader fonteHTML = new BufferedReader(new InputStreamReader(url.openStream()));
             String linhaHTML, regexForDate = null, regexForPDF = null;
-            Date date = new Date();
+            Date date = null;
 
             while ((linhaHTML = fonteHTML.readLine()) != null) {
 
@@ -98,6 +101,58 @@ public class Main {
 
     // Busca de diário oficial em http://www.diariooficial.pi.gov.br/diarios.php
     public static void getDiariosEmDiarioOficialPI() {
+        try {
+            URL url = new URL(DIARIO_OFICIAL_PI);
+            BufferedReader fonteHTML = new BufferedReader(new InputStreamReader(url.openStream()));
+            String linhaHTML, regexForDate = null, regexForPDF = null;
+            Date date = null;
+            String dateString = null;
+            Matcher matcher = null;
+
+            // Encontra a data do ultimo diário publicado
+            buscaData:
+            while ((linhaHTML = fonteHTML.readLine()) != null) {
+
+                regexForDate = "\\d{2}/\\d{2}/\\d{4}";
+
+                Pattern datePattern = Pattern.compile(regexForDate);
+                matcher = datePattern.matcher(linhaHTML);
+
+                while (matcher.find()) {
+                    date = convertDate(matcher.group());
+                    dateString = matcher.group();
+                    // Ao encontrar a data sai do loop mais externo
+                    break buscaData;
+                }
+            }
+            fonteHTML.close();
+
+            // Constrói nova url para coletar o PDF e Busca em nova fonte
+            String arrayData[] = dateString.split("/");
+            url = new URL(DIARIO_OFICIAL_PI_ESPECIFICA+arrayData[2]+arrayData[1]+arrayData[0]);
+            fonteHTML = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            buscaPDF:
+            while ((linhaHTML = fonteHTML.readLine()) != null) {
+                regexForPDF = "DIARIO+[0-9]+[_]+[0-9A-Za-z]+[.][Pp][Dd][Ff]";
+                Pattern pdfPattern = Pattern.compile(regexForPDF);
+
+                matcher = pdfPattern.matcher(linhaHTML);
+                while (matcher.find()) {
+                    if (!isNull(date)) {
+                        diarios.add(new Diarios(date, matcher.group()));
+                        // Ao encontrar o pdf sai do loop mais externo
+                        break buscaPDF;
+                    }
+                }
+            }
+            fonteHTML.close();
+
+        } catch(MalformedURLException excecao){
+            excecao.printStackTrace();
+        } catch(IOException excecao){
+            excecao.printStackTrace();
+        }
 
     }
 
@@ -106,11 +161,11 @@ public class Main {
 
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         // Vetor de url das fontes
         String[] fontes = new String[]{
-         //       DIARIO_OFICIAL_PI,
+                DIARIO_OFICIAL_PI,
          //       DIARIO_OFICIAL_DOS_MUNICIPIOS,
                 DOM_TERESINA,
                 DOM_PARNAIBA
